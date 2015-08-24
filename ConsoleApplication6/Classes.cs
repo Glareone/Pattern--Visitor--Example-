f8952070605749b700742174fd63fe7cd17e4dbe
Glareone <enlarius@gmail.com>
2015-08-24 13:53:56 +0300

    commit

---
+++ ConsoleApplication6/Classes.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+        public interface ILogEntryVisitor
+        {
+            void Visit(ExceptionLogEntry exceptionLogEntry);
+            void Visit(SimpleLogEntry simpleLogEntry);
+        }
+
+        public abstract class LogEntry
+        {
+            public abstract void Accept(ILogEntryVisitor logEntryVisitor);
+            // Остальные члены остались без изменения
+        }
+
+
+        public class ExceptionLogEntry : LogEntry
+        {
+            public override void Accept(ILogEntryVisitor logEntryVisitor)
+            {
+                // Благодаря перегрузке методов выбирается метод
+                // Visit(ExceptionLogEntry)
+                logEntryVisitor.Visit(this);
+            }
+        }
+
+        public class SimpleLogEntry : LogEntry
+        {
+            public override void Accept(ILogEntryVisitor logEntryVisitor)
+            {
+                logEntryVisitor.Visit(this);
+            }
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public class DatabaseLogSaver : ILogEntryVisitor
+        {
+            public void SaveLogEntry(LogEntry logEntry)
+            {
+                logEntry.Accept(this);
+            }
+
+            void ILogEntryVisitor.Visit(ExceptionLogEntry exceptionLogEntry)
+            {
+                SaveException(exceptionLogEntry);
+            }
+
+            void ILogEntryVisitor.Visit(SimpleLogEntry simpleLogEntry)
+            {
+                SaveSimpleLogEntry(simpleLogEntry);
+            }
+
+            private void SaveException(ExceptionLogEntry logEntry) { Console.WriteLine("Exception was saved");}
+            private void SaveSimpleLogEntry(SimpleLogEntry logEntry) { Console.WriteLine("Simple log was saved");}
+        }
+}
---
+++ ConsoleApplication6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExceptionLogEntry exlogEntry = new ExceptionLogEntry();
+            SimpleLogEntry simpleLogEntry = new SimpleLogEntry();
+
+            var dbsaver = new DatabaseLogSaver();
+            dbsaver.SaveLogEntry(simpleLogEntry);
+            dbsaver.SaveLogEntry(exlogEntry);
+            Console.ReadLine();
+        }
+    }
+}
